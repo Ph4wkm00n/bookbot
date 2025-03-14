1ebf7d0f0b36adc50e4b3ed1b790d66055947a2b
Ph4wkm00n <ph4wkm00n@gmail.com>
2025-03-14 12:33:53 +0800

    new updates 14 Mar 25

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    # print(text)
-    word_count = count_words(book_text)
-    # print("Word count:", word_count)
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-    counts = count_alpha_chars(book_text)
-    """
-    print("Character counts:")
-    for char, count in counts.items():
-        # Use repr(char) to clearly show spaces and special characters.
-        print(f"{repr(char)}: {count}")
-    """
-# Print the report
-    print("Report:")
-    print("Total words:", word_count)
-    print("Alphabet character counts:")
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     
-    # Sort keys alphabetically for a neat report
-    for char in sorted(counts.keys()):
-        print(f"The '{char}'character was found {counts[char]} times")
+    # book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words(text):
-    
-    #Counts the number of words in the given text.
-    # Split the text by whitespace and count the resulting parts.
-    words = text.split()
-    return len(words)
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
 
-def count_alpha_chars(book_text): 
-    
-    #Counts the frequency of each character in the given text.
-    # Initialize an empty dictionary to store character counts.
-    char_count = {}
-    
-    # Convert the entire text to lowercase and iterate through each character.
-    for char in book_text.lower():
-        
-        if char.isalpha():  # Only count alphabet characters
-            char_count[char] = char_count.get(char, 0) + 1
-    
-    return char_count
-
+    print("============= END ===============")
 
 
 main()
